3b192aaeb005924bd2d235c4586087c6f7482849
giambi-columbus <giambi@columbus.finance>
2023-07-20 13:18:26 +0800

    feat: update campaign link

--- src/config/constants.ts
+++ src/config/constants.ts
@@ -2,7 +2,7 @@
 export const URLS = {
   BANNER: 'https://galxe.com/scubaprotocol',
   DAPP: 'https://testnet.scuba.finance/',
-  CAMPAIGN: 'https://galxe.com/scubaprotocol/campaigns',
+  CAMPAIGN: 'https://hickory-tarragon-576.notion.site/df81f0f35f2a452abd09f01f0579ca3c?v=b133eaafcae242fbb727836f427eb30b',
   GITBOOK: 'https://scuba-protocol.gitbook.io/scuba-protocol/',
   HOW_IT_WORKS: 'https://scuba-protocol.gitbook.io/scuba-protocol/our-technology/how-it-works',
   HOW_IT_WORKS_FEATURES: 'https://scuba-protocol.gitbook.io/scuba-protocol/our-technology/how-it-works/the-scuba-network-features'
